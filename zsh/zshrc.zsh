4e239f84c1f0eaae359ef17329bf83f357e183b2
uglyboy <uglyboy@uglyboy.cn>
2022-01-26 18:28:09 +0800

    add local file of zsh

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,3 +1,8 @@
+# Allow local customizations in the ~/.zshrc_local_before file
+if [ -f ~/.zshrc_local_before ]; then
+    source ~/.zshrc_local_before
+fi
+
 # environment variables
 export DOTFILES="$HOME/.dotfiles"
 export PATH=$PATH:$HOME/.local/bin
@@ -7,9 +12,6 @@ if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
-# Config of Plugin TMUX
-ZSH_TMUX_AUTOSTART=true
-ZSH_TMUX_DEFAULT_SESSION_NAME=ssh
 
 # load zgen
 source ~/.zgen/zgen.zsh
@@ -37,7 +39,11 @@ fi
 source $DOTFILES/zsh/p10k.zsh
 
 # alias definitions
-# 查看文件夹大小
 alias size='f(){ du -h --max-depth=1 $1 | sort -hr; }; f'
 alias auto='systemctl list-unit-files --type=service | grep enabled | more'
 alias cp='rsync -av --progress'
+
+# Allow local customizations in the ~/.zshrc_local_after file
+if [ -f ~/.zshrc_local_after ]; then
+    source ~/.zshrc_local_after
+fi
