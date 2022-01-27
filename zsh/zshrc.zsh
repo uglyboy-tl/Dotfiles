3791fceea55d34a99bf9da7ddf469f06e6d8572f
uglyboy <uglyboy@uglyboy.cn>
2022-01-27 09:31:33 +0800

    update

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,11 +1,10 @@
 # Allow local customizations in the ~/.zshrc_local_before file
-if [ -f ~/.zshrc_local_before ]; then
-    source ~/.zshrc_local_before
-fi
+[ ! -f ~/.zshrc_local_before ] || source ~/.zshrc_local_before
 
 # environment variables
 export DOTFILES="$HOME/.dotfiles"
 export PATH=$PATH:$HOME/.local/bin
+export OS_RELEASE=`awk -F= '/^NAME/{print $2}' /etc/os-release | sed 's/"//g'`
 
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
@@ -24,7 +23,16 @@ if ! zgen saved; then
     zgen oh-my-zsh plugins/git
     zgen oh-my-zsh plugins/git-auto-fetch
     zgen oh-my-zsh plugins/extract
-    zgen oh-my-zsh plugins/debian
+    case ${OS_RELEASE} in
+        "Ubuntu" )
+	    zgen oh-my-zsh plugins/ubuntu
+        ;;
+        "Raspbian GNU/Linux" )
+            zgen oh-my-zsh plugins/debian
+        ;;
+        * )
+	    echo "other"
+    esac
     zgen oh-my-zsh plugins/tmux
     zgen oh-my-zsh plugins/autojump
     zgen load zsh-users/zsh-syntax-highlighting
@@ -44,6 +52,4 @@ alias auto='systemctl list-unit-files --type=service | grep enabled | more'
 alias cp='rsync -av --progress'
 
 # Allow local customizations in the ~/.zshrc_local_after file
-if [ -f ~/.zshrc_local_after ]; then
-    source ~/.zshrc_local_after
-fi
+[ ! -f ~/.zshrc_local_after ] || source ~/.zshrc_local_after
