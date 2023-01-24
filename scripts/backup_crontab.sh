0b212c0fc576a40d7037a69750c47a1fc810ccb1
uglyboy <uglyboy@uglyboy.cn>
2023-01-24 19:10:18 +0800

    add crontab backup

---
+++ scripts/backup_crontab.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+BACKUP_DIRECTORY="${HOME}/Backup/cron"
+
+if [ ! -e "${BACKUP_DIRECTORY}" ]; then
+	        mkdir -p ${BACKUP_DIRECTORY}
+fi
+
+crontab -l > ${BACKUP_DIRECTORY}/$(date '+%Y%m%d').txt
+find ${HOME}/crontab_backup -mtime +7 -name '*.txt' -exec rm -rf {} \;
+
+rsync -av --delete ${BACKUP_DIRECTORY} uglyboy.cn:/srv/Base/Data/Remote
