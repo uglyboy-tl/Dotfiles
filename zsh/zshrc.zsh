89303d3c57a1b2becc71bc5d010d29cde8882180
uglyboy <uglyboy@uglyboy.cn>
2023-02-05 11:18:07 +0800

    update

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -3,7 +3,7 @@ export PATH=$PATH:$HOME/.local/bin
 
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+    source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
 # Allow local customizations in the ~/.zshrc_local_before file
