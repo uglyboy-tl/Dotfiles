2155677c5ffc3f86904448c5fdbfbe0e619ff844
Uglyboy <25730357+uglyboy-tl@users.noreply.github.com>
2024-11-22 23:26:14 +0800

    🐛 之前的环境变量未生效，已修复

--- config/mutt/get_pass.py
+++ config/mutt/get_pass.py
@@ -1,5 +1,8 @@
 #! /usr/bin/env python3
 from subprocess import check_output
+import os
 
 def get_pass(username):
-    return check_output(f"gpg -dq ~/.local/share/password/{username}.gpg", shell=True).rstrip(b"\n")
+    env = os.environ.copy()
+    env['GNUPGHOME'] = '~/.local/share/gnupg'
+    return check_output(f"gpg -dq ~/.local/share/password/{username}.gpg", env=env, shell=True).rstrip(b"\n")
