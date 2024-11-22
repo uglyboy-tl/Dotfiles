2c5d4a59f4249c91afa102df13f45d4904a86ec1
Uglyboy <25730357+uglyboy-tl@users.noreply.github.com>
2024-11-22 22:44:21 +0800

    🐛 修复 mutt 相关功能

--- config/mutt/get_pass.py
+++ config/mutt/get_pass.py
@@ -2,4 +2,4 @@
 from subprocess import check_output
 
 def get_pass(username):
-    return check_output(f"gpg -dq ~/.password/{username}.gpg", shell=True).rstrip(b"\n")
+    return check_output(f"gpg -dq ~/.local/share/password/{username}.gpg", shell=True).rstrip(b"\n")
