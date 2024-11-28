0f20b427e8cd7a41b225b894cf7e85904ce413b0
uglyboy-tl <25730357+uglyboy-tl@users.noreply.github.com>
2024-11-28 13:08:20 +0800

    ✨ feat(config): 添加 lf 文件管理器配置
    - 新增 lf 配置文件夹，包含 cleaner、colors、icons、lfrc 和 scope 文件
    - 添加 update_lf.sh 脚本，用于更新 lf 文件管理器
    - 在 install.conf.yaml 中添加 lf 配置项

---
+++ scripts/update_lf.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# debug output and exit on error or use of undeclared variable or pipe error:
+set -o xtrace -o errtrace -o errexit -o nounset -o pipefail
+
+# set proxy
+export http_proxy="http://192.168.0.100:20171"
+export https_proxy="http://192.168.0.100:20171"
+
+version="$(curl --location --head https://github.com/gokcehan/lf/releases/latest | grep -i location: | sed 's/^.*\/tag\/\([^\/]*\)\r$/\1/')"
+filename="lf-linux-arm64.tar.gz"
+uri_to_download="https://github.com/gokcehan/lf/releases/download/${version}/${filename}"
+
+curl --fail --show-error --location "$uri_to_download" |
+  tar -xz -C /tmp/
+mv --force /tmp/lf "${HOME}/.local/bin/lf" &&
+  chmod a+x "${HOME}/.local/bin/lf"
+
+curl --fail --show-error --location https://raw.githubusercontent.com/gokcehan/lf/master/lf.1 --output "$HOME"/.local/share/man/man1/lf.1
+
+# stop proxy
+unset http_proxy
+unset https_proxy
+
+if [ ! -x "${HOME}/.local/bin/lf" ]; then
+    echo '"lf" was not successfully installed!' >&2
+    # DISPLAY=:0 notify-send --urgency=critical "Failed updating lf!
+      # Run $0 to check."
+    exit 2
+fi
