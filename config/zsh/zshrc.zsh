d2f74717bbf228643f6dc838a8212a62ef49fa41
Uglyboy <25730357+uglyboy-tl@users.noreply.github.com>
2025-02-13 07:17:22 +0800

    feat(config): 添加 Bash 配置文件并更新 Zsh 配置

--- config/zsh/zshrc.zsh
+++ config/zsh/zshrc.zsh
@@ -1,17 +1,14 @@
-# If you come from bash you might have to change your $PATH.
-export PATH=$HOME/.local/bin:$PATH
-[ ! -f ${XDG_CONFIG_HOME:-$HOME/.config}/environment ] || source ${XDG_CONFIG_HOME:-$HOME/.config}/environment
-export ZDOTDIR=${ZDOTDIR:-$HOME/.config/zsh}
-export ZLOCAL=${XDG_CONFIG_HOME:-$HOME/.config}/local
-
+export PATH="$HOME/.local/bin:$PATH"
+[ -f "${XDG_CONFIG_HOME:-$HOME/.config}/environment" ] && source "${XDG_CONFIG_HOME:-$HOME/.config}/environment"
+export ZDOTDIR="${ZDOTDIR:-$HOME/.config/zsh}"
+export ZLOCAL="${XDG_CONFIG_HOME:-$HOME/.config}/local"
+export HISTFILE=$XDG_STATE_HOME/zsh/history
 export ZSH_DISABLE_COMPFIX=true
 
-# Allow local customizations in the ~/.zshrc_local_before file
-[ ! -f $ZLOCAL/zshrc.before ] || source $ZLOCAL/zshrc.before
+[ -f "$ZLOCAL/zshrc.before" ] && source "$ZLOCAL/zshrc.before"
 
-# environment variables
 export DOTFILES="$XDG_DATA_HOME/dotfiles"
-export OS_RELEASE=`awk -F= '/^NAME/{print $2}' /etc/os-release | sed 's/"//g'`
+export OS_RELEASE=$(awk -F= '/^NAME/{print $2}' /etc/os-release | tr -d '"')
 export PYTHON_VENV_NAME=".venv"
 
 # fzf options
@@ -20,62 +17,47 @@ export FZF_DEFAULT_OPTS="--height 50% --preview-window right:60% --layout=revers
 export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!{.git,.idea,.vscode,.sass-cache,node_modules,build,.m2}/*" 2> /dev/null'
 export FZF_ALT_C_COMMAND="rg --sort-files --files --null 2> /dev/null | xargs -0 dirname | uniq"
 
-# load zgen
-export ZGEN_DIR=$ZDOTDIR/zgen
-source $ZGEN_DIR/zgen.zsh
+export ZGEN_DIR="$ZDOTDIR/zgen"
+source "$ZGEN_DIR/zgen.zsh"
 
 # Generate zgen init.sh if it doesn't exist
 if ! zgen saved; then
     zgen oh-my-zsh
-
     zgen oh-my-zsh plugins/git
     zgen oh-my-zsh plugins/git-auto-fetch
     zgen oh-my-zsh plugins/extract
-    case ${OS_RELEASE} in
-        "Ubuntu" )
+    case "$OS_RELEASE" in
+        "Ubuntu"|"Raspbian GNU/Linux"|"Debian GNU/Linux")
             zgen oh-my-zsh plugins/ubuntu
             ;;
-        "Raspbian GNU/Linux" )
-            zgen oh-my-zsh plugins/ubuntu
+        "Arch Linux"|"Arch Linux ARM")
+        zgen oh-my-zsh plugins/archlinux
             ;;
-        "Debian GNU/Linux" )
-            zgen oh-my-zsh plugins/ubuntu
+        *)
             ;;
-        "Arch Linux" )
-            zgen oh-my-zsh plugins/archlinux
-            ;;
-        "Arch Linux ARM" )
-            zgen oh-my-zsh plugins/archlinux
-            ;;
-        * )
-            echo "other"
-            ;;
-    esac
+        esac
+
     zgen oh-my-zsh plugins/cp
     zgen oh-my-zsh plugins/zoxide
     zgen oh-my-zsh plugins/fzf
     zgen load zsh-users/zsh-syntax-highlighting
     zgen load wbingli/zsh-wakatime
 
-    # Plugins
-    [ ! -f $ZLOCAL/zshrc.plugins ] || source $ZLOCAL/zshrc.plugins
+    [ -f "$ZLOCAL/zshrc.plugins" ] && source "$ZLOCAL/zshrc.plugins"
 
-    # Generate init.sh
     zgen save
 fi
 
-compinit -d "$XDG_CACHE_HOME"/zsh/zcompdump-"$ZSH_VERSION"
-
-# replaced command
+compinit -d "$XDG_CACHE_HOME/zsh/zcompdump-$ZSH_VERSION"
 alias duf='duf --only local'
-alias wget=wget --hsts-file="$XDG_DATA_HOME/wget-hsts"
+alias wget='wget --hsts-file="$XDG_DATA_HOME/wget-hsts"'
 
-# custom alias
-[ ! -f $ZDOTDIR/zshrc.alias ] || source $ZDOTDIR/zshrc.alias
-
-# Allow local customizations in the ~/.zshrc_local_after file
-[ ! -f $ZLOCAL/zshrc.after ] || source $ZLOCAL/zshrc.after
+[ -f "$ZDOTDIR/zshrc.alias" ] && source "$ZDOTDIR/zshrc.alias"
+[ -f "$ZLOCAL/zshrc.after" ] && source "$ZLOCAL/zshrc.after"
 
 setopt no_nomatch
 
 eval "$(starship init zsh)"
+
+
+
