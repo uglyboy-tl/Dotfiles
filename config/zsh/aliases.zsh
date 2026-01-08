eae2e33160f5cf2a3965052cc8ad4410d95c0dc9
uglyboy-tl <25730357+uglyboy-tl@users.noreply.github.com>
2026-01-08 10:20:10 +0800

    feat(config): 添加 bat 主题和 fastfetch 徽标，优化环境变量和包管理源
    - 新增 bat 配置文件和 Catppuccin Frappe 主题
    - 添加 fastfetch Ganyu 徽标
    - 优化环境变量配置，添加 npm 和 Java 相关设置
    - 更新 pip 和 uv 包管理源为清华镜像
    - 改进 git、starship、tmux 和 zsh 配置
    - 添加代理相关别名和工具
    - 更新 dotbot 子模块和安装配置文件

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -1,2 +1,32 @@
 alias size='f(){ sudo du -h --max-depth=1 $1 | sort -hr; }; f'
 alias runv='source .venv/bin/activate'
+alias duf='duf --only local'
+alias wget='wget --hsts-file="$XDG_DATA_HOME/wget-hsts"'
+alias fetch='fastfetch'
+
+__ZSHPROXY_HTTP="${__ZSHPROXY_HTTP:-nas.uglyboy.cn:50172}"
+
+proxy () {
+	# http_proxy
+	export http_proxy="${__ZSHPROXY_HTTP}"
+	export HTTP_PROXY="${__ZSHPROXY_HTTP}"
+	# https_proxy
+	export https_proxy="${__ZSHPROXY_HTTP}"
+	export HTTPS_PROXY="${__ZSHPROXY_HTTP}"
+    echo "HTTP Proxy On."
+}
+
+# cmd: unset proxy
+noproxy () {
+	unset http_proxy
+	unset HTTP_PROXY
+	unset https_proxy
+	unset HTTPS_PROXY
+    echo "HTTP Proxy Off."
+}
+
+# cmd: show proxy
+show_proxy () {
+    echo "http_proxy: $__ZSHPROXY_HTTP"
+    echo "https_proxy: $__ZSHPROXY_HTTP"
+}
